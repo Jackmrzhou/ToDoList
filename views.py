ed001bba712436912bb60fb2adaf9554089bbc2f
Jackmrzhou <594780735@qq.com>
2017-02-07 22:15:50 +0800

    add logout and fix login

--- views.py
+++ views.py
@@ -1,11 +1,11 @@
 import forms
-from flask import Flask,render_template,redirect,flash, request
+from flask import Flask,render_template,redirect,flash, request,session
 from flask_sqlalchemy import SQLAlchemy
 from functools import wraps
 
 app = Flask(__name__)
 app.config.from_pyfile('config.py')
-app.debug=True
+app.debug = True
 db = SQLAlchemy(app)
 
 import db_operate
@@ -16,8 +16,7 @@ global_user = None
 def is_user(func):
 	@wraps(func)
 	def wrapper(*args, **kw):
-		global global_user
-		if not global_user:
+		if 'username' not in session:
 			return redirect('/login')
 		return func(*args, **kw)
 	return wrapper
@@ -26,8 +25,9 @@ def is_user(func):
 @app.route('/', methods = ['GET','POST'])
 @is_user
 def index():
+	now_user = db_operate.query_user(session['username'])
 	form = forms.thing()
-	stuffs = db_operate.query_all(user = global_user)
+	stuffs = db_operate.query_all(user = now_user)
 
 	if request.form:
 		f = request.form.to_dict()
@@ -42,14 +42,15 @@ def index():
 		if form.validate():
 			db_operate.insert(stuff= form.stuff.data,
 				time= form.time.data,
-				user = global_user)
+				user = now_user)
 			return redirect('/')
 		else:
 			flash('Check Again!')
 
 	return render_template('index.html',
 		form = form,
-		stuffs = stuffs)
+		stuffs = stuffs,
+		username = now_user.username)
 
 @app.route('/register', methods = ['GET', 'POST'])
 def register():
@@ -62,8 +63,7 @@ def register():
 		#以后再将密码加密
 		if password == repeat_password:
 			db_operate.insert(username = username, password = password)
-			global global_user 
-			global_user= db_operate.query_user(username = username)
+			session['username'] = username
 			return redirect('/')
 		else:
 			flash('Check Again!')
@@ -84,8 +84,8 @@ def login():
 		user = db_operate.query_user(form.username.data)
 		if user:
 			if form.password.data == user.password:
-				global global_user 
-				global_user = user
+				session['username'] = form.username.data
+
 				return redirect('/')
 			else:
 				flash('Password is not correct!')
@@ -96,6 +96,11 @@ def login():
 	
 	return render_template('login.html',
 		form = form)
+
+@app.route('/logout', methods =['GET'])
+def logout():
+	session.pop('username', None)
+	return redirect('/login')
 
 if __name__ == '__main__':
 	app.run()
